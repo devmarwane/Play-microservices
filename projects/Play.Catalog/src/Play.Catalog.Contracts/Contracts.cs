b6344a8b36de3f6bb0acce9511a90c0013f4c9d2
Marwane Bidamou <devr.marwane@gmail.com>
2023-01-28 16:15:16 +0100

    Preparing Message Broker : Defining the message contracts

---
+++ projects/Play.Catalog/src/Play.Catalog.Contracts/Contracts.cs
@@ -0,0 +1,8 @@
+﻿namespace Play.Catalog.Contracts
+{
+    public record CatalogItelCreated(Guid ItemId, string Name, string Description);
+
+    public record CatalogItelUpdated(Guid ItemId, string Name, string Description);
+
+    public record CatalogItelDeleted(Guid ItemId);
+}
