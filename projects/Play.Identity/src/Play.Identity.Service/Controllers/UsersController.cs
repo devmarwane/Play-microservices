852c78538a61cacb97549010911ad461ca377770
Marwane Bidamou <devr.marwane@gmail.com>
2023-02-11 20:01:06 +0100

    Play.Identity : Adding the Users REST API

---
+++ projects/Play.Identity/src/Play.Identity.Service/Controllers/UsersController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Play.Identity.Service.Dtos;
+using Play.Identity.Service.Entities;
+using Play.Identity.Service.Extensions;
+
+namespace Play.Identity.Service.Controllers
+{
+    [Route("users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<UserDto>> Get()
+        {
+            var users = _userManager.Users
+                .ToList()
+                .Select(user => user.AsDto());
+
+            return Ok(users);
+        }
+
+        // users/{123}
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserDto>> GetByIdAsync(Guid id)
+        {
+            var user = _userManager.Users.Where(x => x.Id == id)
+                .FirstOrDefault();
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user.AsDto();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAsync(Guid id, UpdateUserDto userDto)
+        {
+            var user = _userManager.Users.Where(x => x.Id == id)
+                .FirstOrDefault();
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            user.Email = userDto.Email;
+            user.UserName = userDto.Email;
+            user.Gil = userDto.Gil;
+
+            await _userManager.UpdateAsync(user);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(Guid id)
+        {
+            var user = _userManager.Users.Where(x => x.Id == id)
+                .FirstOrDefault();
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            await _userManager.DeleteAsync(user);
+
+            return NoContent();
+        }
+    }
+}
