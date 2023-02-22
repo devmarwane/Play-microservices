13362bf452877ca2b46aa0521f5f2c14dcafcc37
Marwane Bidamou <devr.marwane@gmail.com>
2023-02-23 00:43:33 +0100

    Preparing the Saga participants - Debiting gil via asynchronous messages

---
+++ projects/Play.Identity/src/Play.Identity.Contracts/Contracts.cs
@@ -0,0 +1,15 @@
+﻿namespace Play.Identity.Contracts
+{
+    public record DebitGil
+        (
+        Guid UserId,
+        decimal Gil,
+        Guid CorrelationId
+        );
+
+    public record GilDebited
+        (
+        Guid CorrelationId
+        );
+
+}
--- projects/Play.Identity/src/Play.Identity.Service/Program.cs
+++ projects/Play.Identity/src/Play.Identity.Service/Program.cs
@@ -1,10 +1,13 @@
+using GreenPipes;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
+using Play.Common.MassTransit;
 using Play.Common.Settings;
 using Play.Identity.Service.Entities;
+using Play.Identity.Service.Exceptions;
 using Play.Identity.Service.HostedServices;
 using Play.Identity.Service.Settings;
 using System.Configuration;
@@ -26,6 +29,12 @@ builder.Services.Configure<IdentitySettings>(builder.Configuration.GetSection(na
         mongoDbSettings.ConnectionString,
         serviceSettings.ServiceName
     );
+
+builder.Services.AddMassTrannsitWithRabbitMq(retryConfigurator =>
+{
+    retryConfigurator.Interval(3, TimeSpan.FromSeconds(5));
+    retryConfigurator.Ignore(typeof(UnknownUserException),typeof(UnsufficientFundsException));
+});
 
 builder.Services.AddIdentityServer(options =>
 {
