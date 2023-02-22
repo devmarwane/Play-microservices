22dda645b8c3651f4e27ce97036885ae681a6165
Marwane Bidamou <devr.marwane@gmail.com>
2023-02-22 22:10:23 +0100

    Preparing the Saga participants - Allowing services to customize the retry configuration

--- projects/Play.Common/src/Play.Common/MassTransit/Extensions.cs
+++ projects/Play.Common/src/Play.Common/MassTransit/Extensions.cs
@@ -1,6 +1,8 @@
 ﻿using GreenPipes;
+using GreenPipes.Configurators;
 using MassTransit;
 using MassTransit.Definition;
+using MassTransit.ExtensionsDependencyInjectionIntegration;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Play.Common.Settings;
@@ -15,7 +17,10 @@ namespace Play.Common.MassTransit
 {
     public static class Extensions
     {
-        public static IServiceCollection AddMassTrannsitWithRabbitMq(this IServiceCollection services)
+        public static IServiceCollection AddMassTrannsitWithRabbitMq(
+            this IServiceCollection services,
+            Action<IRetryConfigurator>? configureRetries = null
+            )
         {
             services.AddMassTransit(configure =>
             {
@@ -24,29 +29,40 @@ namespace Play.Common.MassTransit
                 //we gonna register consumers by define the assembly that should have all the consumers already defined, and that's going to be the entry assemby for wichever microservice is invoking this class
                 configure.AddConsumers(Assembly.GetEntryAssembly());
 
-                configure.UsingRabbitMq((context, configurator) =>
-                {
-                    var configuration = context.GetService<IConfiguration>();
-                    //service settings
-                    var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
-
-                    var rabbitMQSettings = configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
-                    configurator.Host(rabbitMQSettings.Host);
-                    configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
-
-                    //in case there is a trouble in executing consumers code
-                    configurator.UseMessageRetry(retryConfigurator =>
-                    {
-                        retryConfigurator.Interval(retryCount: 3, interval: TimeSpan.FromSeconds(5));
-                        //with that logic, anytime a message is not able to be consumed by a consumer, it'll be retried 3 times with a 5 seconds delay
-                    });
-                });
+                configure.UsingPlayEconomyRabbitMQ(configureRetries);
             });
 
             services.AddMassTransitHostedService();
 
 
             return services;
+        }
+
+        public static void UsingPlayEconomyRabbitMQ(
+            this IServiceCollectionBusConfigurator configure,
+            Action<IRetryConfigurator>? configureRetries = null
+            )
+        {
+            configure.UsingRabbitMq((context, configurator) =>
+            {
+                var configuration = context.GetService<IConfiguration>();
+                //service settings
+                var serviceSettings = configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
+
+                var rabbitMQSettings = configuration.GetSection(nameof(RabbitMQSettings)).Get<RabbitMQSettings>();
+                configurator.Host(rabbitMQSettings.Host);
+                configurator.ConfigureEndpoints(context, new KebabCaseEndpointNameFormatter(serviceSettings.ServiceName, false));
+
+                if (configureRetries == null)
+                {
+                    configureRetries = (retryConfigurator) => {
+                        retryConfigurator.Interval(retryCount: 3, interval: TimeSpan.FromSeconds(5));
+                        //with that logic, anytime a message is not able to be consumed by a consumer, it'll be retried 3 times with a 5 seconds delay
+                    };                  
+                }
+
+                configurator.UseMessageRetry(configureRetries);
+            });
         }
     }
 }
