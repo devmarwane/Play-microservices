c0cdb37c0f3eb5d150bb4fb16ccad77b6b97130f
Marwane Bidamou <devr.marwane@gmail.com>
2023-02-23 23:52:21 +0100

    Implementing the Puchase Saga : Creating a MassTransit state machine

---
+++ projects/Play.Trading/src/Play.Trading.Service/StateMachines/PurchaseStateMachine.cs
@@ -0,0 +1,17 @@
+﻿using Automatonymous;
+
+namespace Play.Trading.Service.StateMachines
+{
+    public class PurchaseStateMachine : MassTransitStateMachine<PurchaseState>
+    {
+        public State Accepted { get; set; }
+        public State ItemsGranted { get; set; }
+        public State Completed { get; set; }
+        public State Faulted { get; set; }
+
+        public PurchaseStateMachine()
+        {
+            InstanceState(state => state.CurrentState);
+        }
+    }
+}
