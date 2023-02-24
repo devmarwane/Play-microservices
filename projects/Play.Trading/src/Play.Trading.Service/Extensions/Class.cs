0d4e10e52f6b095d038dc23a3e23af451bfbfa42
Marwane Bidamou <devr.marwane@gmail.com>
2023-02-24 23:01:51 +0100

    Implementing the Puchase Saga : Configuring the Trading microservice

---
+++ projects/Play.Trading/src/Play.Trading.Service/Extensions/Class.cs
@@ -0,0 +1,22 @@
+﻿using MassTransit;
+using Play.Common.MassTransit;
+using Play.Trading.Service.StateMachines;
+
+namespace Play.Trading.Service.Extensions
+{
+    public class Class
+    {
+        public static void AddMassTransit(IServiceCollection services)
+        {
+            services.AddMassTransit(configure =>
+            {
+                configure.UsingPlayEconomyRabbitMQ();
+                configure.AddSagaStateMachine<PurchaseStateMachine, PurchaseState>()
+                    .MongoDbRepository(r =>
+                    {
+                        var serviceSettings = Conf
+                    });
+            });
+        }
+    }
+}
--- projects/Play.Trading/src/Play.Trading.Service/Program.cs
+++ projects/Play.Trading/src/Play.Trading.Service/Program.cs
@@ -1,3 +1,11 @@
+using MassTransit;
+using Microsoft.IdentityModel.Tokens;
+using Play.Common.Identity;
+using Play.Common.MassTransit;
+using Play.Common.MongoDB;
+using Play.Common.Settings;
+using Play.Trading.Service.StateMachines;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +14,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddMongo()
+    .AddJwtBearerAuthentication();
+
+AddMassTransit(builder.Services);
 
 var app = builder.Build();
 
@@ -18,8 +31,32 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
+
+void AddMassTransit(IServiceCollection services)
+{
+    services.AddMassTransit(configure =>
+    {
+        configure.UsingPlayEconomyRabbitMQ();
+        configure.AddSagaStateMachine<PurchaseStateMachine, PurchaseState>()
+            .MongoDbRepository(r =>
+            {
+                var serviceSettings = builder.Configuration.GetSection(nameof(ServiceSettings))
+                                        .Get<ServiceSettings>();
+                var mongoSettings = builder.Configuration.GetSection(nameof(MongoDbSettings))
+                                        .Get<MongoDbSettings>();
+
+                r.Connection = mongoSettings.ConnectionString;
+                r.DatabaseName = serviceSettings.ServiceName;
+            });
+    });
+
+    services.AddMassTransitHostedService();
+}
